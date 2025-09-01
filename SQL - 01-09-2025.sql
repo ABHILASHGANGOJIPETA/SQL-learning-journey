113dd59d7f7350c88e64f5215aa483ab979ae3ae
ABHILASH G <160447475+ABHILASHGANGOJIPETA@users.noreply.github.com>
2025-09-01 20:46:49 +0530

    Database creation and SQL data types practice

---
+++ SQL - 01-09-2025.sql
@@ -0,0 +1,11 @@
+show databases; -- to check databases avalibile in the server
+
+create database excelr;
+
+use excelr; -- get into excelr db
+
+create database pdb; -- removes database permenantly from the server
+drop database pdb;
+
+
+
